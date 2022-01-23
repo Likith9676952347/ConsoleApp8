03255cdf91a7b0031a92a31c461059b95fa1c251
Likith9676952347 <likithkumarreddy143@gmail.com>
2022-01-23 11:18:07 +0530

    Add project files.

---
+++ ConsoleApp8/Animal.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp8
+{
+   public class Animal
+    {
+        public Animal()
+        {
+            Console.WriteLine("Animal constructor");
+        }
+        public void Greet()
+        {
+            Console.WriteLine("Animal Says Hello");
+        }
+        public void Talk()
+        {
+            Console.WriteLine("Animal talk");
+        }
+        public virtual void Sing()
+        {
+            Console.WriteLine("Animal Song");
+        }
+    }
+
+    class Dog : Animal
+    {
+        public Dog()
+        {
+            Console.WriteLine("Dog Constructor");
+        }
+        public override void Sing()
+        {
+            Console.WriteLine("Dog Song");
+        }
+    }
+}
